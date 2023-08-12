bdf59e6d16b98e68ce43e03eac9ff236316eb884
Ritesh Jain <108740484+Jainritgithub@users.noreply.github.com>
2023-08-12 16:04:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+var clutter = ""
+var timer = 6;
+var score = 0;
+var hitNumber = 0;
+
+function makeBubble(){
+    for(var i = 1; i<= 160; i++){
+        var randomNumber =  Math.floor(Math.random()*10)
+         clutter += `<div class="bubble">${randomNumber}</div>`
+         document.querySelector("#p-bottom").innerHTML = clutter
+     }
+}
+
+function getNewHit(){
+   hitNumber =  Math.floor(Math.random()*10)
+  document.querySelector("#hitval").textContent = hitNumber
+
+}
+
+function runtimer(){
+  var timerFunction = setInterval(function(){
+        if(timer > 0){
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        }
+        else{
+            clearInterval(timerFunction)
+            document.querySelector("#p-bottom").innerHTML = `<h2> Game Over ! </h2>`
+             
+        }
+    },1000)
+        
+}
+
+function increaseScore(){
+    document.querySelector("#scoreval").textContent = score;
+}
+
+document.querySelector("#p-bottom").addEventListener("click" , function(dets){
+    var clickedNumber = Number(dets.target.textContent);
+    if(clickedNumber === hitNumber){
+        score += 10;
+        increaseScore();
+        getNewHit();
+        makeBubble();
+    }
+})
+
+makeBubble();
+getNewHit();
+runtimer();
+increaseScore();
